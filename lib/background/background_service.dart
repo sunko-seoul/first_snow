6c1fbbf12a7f4110e23bdb98521adb0b98b6a0b5
Hyungjuk42 <hyungjuk@student.42seoul.kr>
2024-08-16 18:37:55 +0900

    [Feat] get UUID in background and Impl test page close #47

--- lib/background/background_service.dart
+++ lib/background/background_service.dart
@@ -10,6 +10,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import 'dart:io';
 
+
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
